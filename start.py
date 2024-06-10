6728f1e5444a87c5a732127c43ca8d506f1f9ae8
Shaun Prince <suparious@solidrust.net>
2024-06-10 11:37:03 -0700

    enable prometheus exports, for grafana datasource

--- start.py
+++ start.py
@@ -1,5 +1,11 @@
 import multiprocessing
 import os
+import yaml
+from prometheus_client import start_http_server
+
+def load_config():
+    with open('config.yaml', 'r') as file:
+        return yaml.safe_load(file)
 
 def start_metrics_collector():
     os.system('python -m metrics.collector')
@@ -7,13 +13,16 @@ def start_metrics_collector():
 def start_flask_app():
     os.system('gunicorn -c gunicorn_config.py app.main:app')
 
-def start_prometheus_server():
-    os.system('python start_prometheus_server.py')
+def start_prometheus_server(port):
+    start_http_server(port)
 
 def main():
+    config = load_config()
+    metrics_port = config.get('metrics_port', 8000)  # Default to 8000 if not set
+
     p1 = multiprocessing.Process(target=start_metrics_collector)
     p2 = multiprocessing.Process(target=start_flask_app)
-    p3 = multiprocessing.Process(target=start_prometheus_server)
+    p3 = multiprocessing.Process(target=start_prometheus_server, args=(metrics_port,))
 
     p1.start()
     p2.start()
